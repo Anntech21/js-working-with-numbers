adaf846fda44055a165b841dae90c775ec8e315b
Ann Tech <anntech21@gmail.com>
2023-08-10 11:59:48 -0700

    revisit

--- scripts.js
+++ scripts.js
@@ -2,19 +2,27 @@
 // Describe the following code:
 
 let wholesalePrice = 5.45;
+console.log(wholesalePrice);
 let retailPrice = 9.99;
+console.log(retailPrice);
 let quantity = 47;
+console.log(quantity);
 let salesTotal = retailPrice * quantity;
+console.log(salesTotal);
 let profit = salesTotal - (wholesalePrice * quantity);
+console.log(profit);
 
 // Where might this code be used?
-This code migth be used to build a POS system for retail companies, frontend backend interface.
+//This code migth be used to build a POS system for retail companies, frontend backend interface.
 
 // Run this code and log the profit variable in the console. What answer do you get?
 213.38
 
 // How would you change this code to make the value of the quanitity variable dynamic?
 
+userInput = prompt("Enter an amount");
+let dq = parseFloat(userInput);
+console.log(dq);
 
 /********************** Exercise 2 - Operators **********************/
 // Step 1: Calculate the sum of two numbers and log the result.
@@ -27,7 +35,7 @@ console.log("num3");
 9
 
 // Step 2: Calculate the difference between two numbers and print the result.
-let num4 = num1 % num2;
+let num4 = num1 - num2;
 num4
 1
 console.log(num4);
@@ -41,10 +49,16 @@ console.log(num5);
 
 // Step 4: Calculate the result of dividing two numbers and print the result. Handle division by zero. 
 
+num5 = 100 / 5;
+console.log(num5);
+let num6 = 20 / 0
+console.log(num6);
+
+
 //(I GOT CARRIED AWAY INTO PERCENTAGE)
-let englishScore = 60;
-let scienceScore = 76;
-let twoScore = englishScore + scienceScore;
+let artScore = 60;
+let craftScore = 76;
+let twoScore = artScore + craftScore;
 console.log(twoScore);
 let twoSubjectScore = 200;
 console.log(twoSubjectScore);
@@ -52,15 +66,12 @@ console.log(twoSubjectScore);
 let twoSubjectTotalScore = twoScore * 100 / twoSubjectScore;
 let twoSubjectPercentage = twoSubjectTotalScore + '%';
 console.log(twoSubjectPercentage);
-68%
+'68%'
 
 
 // Step 5: Check if a given number is even and print the result.
 let checkScore = twoSubjectPercentage
 if (twoSubjectPercentage % 2 === 0) {console.log ("Its Even")};
-else {console.log ("Its Odd")};
-
- 
 
 
 
@@ -72,6 +83,13 @@ else {console.log ("Its Odd")};
 // Output the result to the screen like so: "You will need [AMOUNT] meals to last you until the age of [AGE]".
 
 
+let currentAge = 39;
+let futureAge = 49;
+let mealsPerDay = 4;
+let mealsPerYear = mealsPerDay * 365;
+console.log(mealsPerYear);
+let mealsPerDecade = mealsPerYear * 10;
+console.log("You will need " +  mealsPerDecade + " meals to last you until the age of " + futureAge); 
 
 
 
@@ -87,7 +105,10 @@ let historyScore = 95;
 // How would you get the average of their scores using JavaScript?
 
 
-
+let totalAverage = mathScore + scienceScore + englishScore + historyScore;
+let averageNum = 4;
+let averageRatio = totalAverage / averageNum;
+console.log(" The average score for this student is " + averageRatio);
 
 
 
@@ -96,6 +117,13 @@ let historyScore = 95;
 
 
 // Prompt the user for the value of the temperature in celcius. 
+userInputCelsius = prompt("enter a temperature in degrees celsius")
+
+userInputCelsius = parseFloat(userInputCelsius);
+
+fahrenheit = ((userInputCelsius * 9/5) + 32);
+
+console.log(userInputCelsius + "°C" + " is equal to " + fahrenheit + "°F");
 
 
 // Run the code so that the following is logged to the console: 25°C is equal to 77°F
@@ -105,10 +133,18 @@ let historyScore = 95;
 /********************** Exercise 6 - Assigning Values  **********************/
 
 // Prompt the user for two numbers.
+let userNumberInput = prompt("Enter a number");
+let userNumberInput2 = prompt("Enter another number");
 
 // Make sure that the values of the variables (the strings) are converted to numbers. 
 
+let userNumberTotal = userNumberInput + userNumberInput2
+console.log("The sum of the two numbers is " + userNumberTotal)
+
 // Create a new variable, add the numbers, and log the value.
+
+userNumberTotal = userNumberInput + userNumberInput2 + 100;
+console.log(userNumberTotal);
 
 // Reassign the variable to store the values of the numbers being substracted. Log the value.
 
@@ -124,6 +160,8 @@ let historyScore = 95;
 /********************** Exercise 7 - Math Object  **********************/
 let decimalNumber = 7.8;
 // Given the above variable, how would you round the value to the nearest whole number (using the Math object)?
+let roundedNumber = Math.round(decimalNumber)
+console.log(roundedNumber);
 
 // Console log the output to check your work.
 
@@ -134,19 +172,22 @@ let decimalNumber = 7.8;
 /********************** Exercise 8 - Math Object  **********************/ // Done
 // Dice Game
 // Create a variable that stores the value of a random number between 1 and 6
-let diceNumber = 6;
 
 // Alert the user of the random number.
+let diceNumber = Math.floor((Math.random() * 6) + 1);
 
-alert("random dice roll is: " + diceNumber;
+alert ("random dice roll is: " + diceNumber);
 
 
 
 
 /********************** Exercise 9 - NaN  **********************/ //-- Done
 let result1 = 0 / 0;
+console.log(result1);
+let result2 = "hello" * 5;
 let result2 = "hello" * 5;
 let result3 = parseInt("abc");
+console.log(result3);
 
 // Log the above variables in the console. What is the output?
 NaN
@@ -154,14 +195,22 @@ NaN
 
 let numVersion = 4;
 let wordVersion = 'four';
+
 let value = numVersion / wordVersion;
 
-value printed out as NaN
+//value printed out as NaN
 
 
 console.log("Four divided by four is: " + (numVersion / wordVersion))
 
 // Look up the isNaN() operator in MDN. What does it do? How would you use it?
+let testVariable = 37;
+isNaN(testVariable);
 
-The NaN global property is a value representing Not-A-Number.
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN
+//The output displayed is "false"
+
+let testVariable2 = "hello";
+isNaN(testVariable2);
+
+//The NaN global property is a value representing Not-A-Number. Answer is in link line below:
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN
